73c9465d389194d6e3f5c992c75104cb1a4019fb
後藤大輝 <daiki@gotoudaikinoMacBook-Air.local>
2019-06-08 14:34:36 +0900

    improve

--- KaeshitaCreator.sol
+++ KaeshitaCreator.sol
@@ -17,14 +17,16 @@ contract KaeshitaCreator is Ownable {
     
     Kaeshita[] kaeshitas;
     
-    function createKaeshita(address _senderAddress, address _recipientAddress, uint _dueDate, uint _price) public {
+    function createKaeshita(address _senderAddress, address _recipientAddress, uint _dueDate, uint _price) public onlyOwner {
         uint id = kaeshitas.push(Kaeshita(_senderAddress, _recipientAddress, now, _dueDate, _price, false));
         
-    emit KaeshitaCreation(_senderAddress, _recipientAddress, _price);
+        emit KaeshitaCreation(_senderAddress, _recipientAddress, id);
+    }
     
-    function getKaeshita(uint _id) public returns (address, address, uint, uint, uint, bool) {
-        Kaeshita newKaeshita = kaeshitas[_id];
+    function getKaeshita(uint _id) public view returns (address, address, uint, uint, uint, bool) {
+        Kaeshita memory newKaeshita = kaeshitas[_id-1];
         
         return (newKaeshita.senderAddress, newKaeshita.recipientAddress, newKaeshita.issueDate, newKaeshita.dueDate, newKaeshita.price, newKaeshita.isReturned);
     }
 }
+
--- KaeshitaPermitter.sol
+++ KaeshitaPermitter.sol
@@ -3,24 +3,22 @@ pragma solidity ^0.5.1;
 import "./KaeshitaCreator.sol";
 
 contract KaeshitaPermitter is KaeshitaCreator{
-    function permitKaeshita(uint _id, bool _isReturned) public returns (bool) {
-        Kaeshita newKaeshita = kaeshitas[_id];
+    event KaeshitaPermittion(address senderAddress, address recipientAddress, uint id);
+    
+    function permitKaeshita(uint _id, bool _isReturned) public onlyOwner returns (bool) {
+        Kaeshita memory newKaeshita = kaeshitas[_id-1];
+        require(_isReturned);
         
-        require(newKaeshita.isreturned == false);
-        
-        if (newKaeshita.isreturned == true) {
-            return true;
+        if (!newKaeshita.isReturned) {
+            kaeshitas[_id-1].isReturned = true;
+            
+            emit KaeshitaPermittion(newKaeshita.senderAddress, newKaeshita.recipientAddress, _id);
         }
         
-        if (_isReturned == false) {
-            return false;
-        }
-        
-        kaeshitas[_id].isreturned = true;
-        return true;
+        return getPermittedData(_id);
     }
     
-    function getPermittedData(uint _id) public returns (bool) {
-        return kaeshitas[_id].isreturned;
+    function getPermittedData(uint _id) public view returns (bool) {
+        return kaeshitas[_id-1].isReturned;
     }
 }
